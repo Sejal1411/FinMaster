a96d2bffb63c131f9f358b194ba959cc9fd4dc9a
Sejal <106868816+Sejal1411@users.noreply.github.com>
2025-05-02 22:47:47 +0530

    Added transactions feature

---
+++ actions/transaction.js
@@ -0,0 +1,46 @@
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/dist/types/server";
+
+export async function createTransaction(data) {
+    try {
+       const { userId } = await auth();
+       if(!userId) throw new Error('Unauthorized');
+
+       const user = await db.user.findUnique({
+          where: { clerkuserId: userId },
+       });
+
+       if(!user) {
+        throw new Error("User not found");
+       }
+
+       const account = await db.account.findUnique({
+        where: {
+            id: data.accountId,
+            userId: user.id,
+        }
+       })
+
+       if(!account) {
+        throw new Error('Account not found');
+       }
+
+       const balanceChange = data.type === "EXPENSE" ? -data.amount : data.amount;
+       const newBalance = account.balance.toNumber() + balanceChange;
+
+       const transaction = await db.$transaction(async(tx) => {
+          const newTransaction = await tx.transaction.create({
+            data: {
+                ...data,
+                userId: user.id,
+                nextRecurringDate: 
+                  data.isRecurring && data.recurringInterval
+                  ? calculateNextRecurringDate(data.date, data.recurringInterval)
+                  : null,
+            },
+          });
+       });
+    } catch(error) {
+
+    }
+}
